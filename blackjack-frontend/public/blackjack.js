1c9ac763d49a148b36082d4a8ab2441366d348ab
Tomas Gübeli <jtomasg@gmail.com>
2019-11-17 16:14:46 -0400

    Update blackjack.js

--- blackjack-frontend/public/blackjack.js
+++ blackjack-frontend/public/blackjack.js
@@ -56,7 +56,7 @@ resetBoard, showBoard, showAlert, getWinner, jQuery, wager */
 			ele   = '',
 			score = '';
 		
-		var guid = 'scjocp3-a9fc';
+		var guid = 'santiago-02c1';
 		var api_payment = 'http://blackjack-payment-blackjack.apps.'+guid+'.open.redhat.com/blackjack';
 		var api_users = 'http://blackjack-users-blackjack.apps.'+guid+'.open.redhat.com/blackjack';
 
