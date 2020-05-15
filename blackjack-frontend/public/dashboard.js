09f849183592d32aff063736a4d5ff140d92bbd2
Tomas Gübeli <jtomasg@gmail.com>
2020-05-15 18:54:48 -0400

    Update dashboard.js

--- blackjack-frontend/public/dashboard.js
+++ blackjack-frontend/public/dashboard.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     var refreshRate = '5000';
     var guid = 'chcompra-2f88';
-    var api_ranking = 'http://blackjack-ranking-blackjack.apps.cluster-'+guid+'.'+guid+'.open.redhat.com/blackjack/user/ranking';
+    
+    var api_ranking = 'http://istio-ingressgateway-user1-istio-system.apps.cluster-'+guid+'.'+guid+'.open.redhat.com/blackjack/user/ranking';
 
     $.getJSON(api_ranking, function (response) {
 
